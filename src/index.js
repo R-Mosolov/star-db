426f3b59e8c7750738cd8fda29830938db4733f7
R-Mosolov <r.v.mosolov@ya.ru>
2019-07-26 22:03:00 +0300

    Lessons 51-52 is done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+    return body;
+};
 
-ReactDOM.render(<h1>Hello, World!</h1>, document.getElementById('root'));
+getResource('https://swapi.co/api/people/1/')
+    .then((body) => {
+        console.log(body);
+    });
