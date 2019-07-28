43acd3190fc478f32626348f9e2050a9412f4cd6
R-Mosolov <r.v.mosolov@ya.ru>
2019-07-28 19:16:54 +0300

    Lesson 56 is done

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,40 +1,74 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+import SwapiService from '../../services/swapi-service';
 
 import './random-planet.css';
 
-const RandomPlanet = () => {
-    return (
-        <nav className="random-planet">
-            <div className="card">
-                <div className="d-flex">
-                    <div>
-                        <img className="card-img m-3 rounded-lg"
-                             alt="Planet Image"
-                             src="https://cdn.mos.cms.futurecdn.net/pNX8eVGowB6WT8tyrTMufk-650-80.jpg"/>
-                    </div>
-                    <div className="d-flex align-items-center">
+export default class RandomPlanet extends Component {
+
+    swapiService = new SwapiService();
+
+    state = {
+        id: null,
+        name: null,
+        population: null,
+        rotationPeriod: null,
+        diameter: null
+    };
+
+    constructor() {
+        super();
+        this.updatePlanet();
+    }
+
+    updatePlanet() {
+        const id = Math.floor(Math.random() * 15) + 2;
+        this.swapiService.getPlanet(id).then((planet) => {
+            this.setState({
+                id,
+                name: planet.name,
+                population: planet.population,
+                rotationPeriod: planet.rotation_period,
+                diameter: planet.diameter
+            });
+        });
+    };
+
+    render() {
+
+        const {id, name, population, rotationPeriod, diameter} = this.state;
+
+        return (
+            <nav className="random-planet">
+                <div className="card">
+                    <div className="d-flex">
                         <div>
-                            <h2>Planet Name</h2>
-                            <table className="table">
-                                <tr className="d-flex">
-                                    <th className="w-100">Population</th>
-                                    <td>123124</td>
-                                </tr>
-                                <tr className="d-flex">
-                                    <th className="w-100">Rotation Period</th>
-                                    <td>43</td>
-                                </tr>
-                                <tr className="d-flex">
-                                    <th className="w-100">Diameter</th>
-                                    <td>100</td>
-                                </tr>
-                            </table>
+                            <img className="card-img m-3 rounded-lg"
+                                 alt="Planet Image"
+                                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}/>
+                        </div>
+                        <div className="d-flex align-items-center">
+                            <div>
+                                <h2>{name}</h2>
+                                <table className="table">
+                                    <tr className="d-flex">
+                                        <th className="w-100">Population</th>
+                                        <td>{population}</td>
+                                    </tr>
+                                    <tr className="d-flex">
+                                        <th className="w-100">Rotation Period</th>
+                                        <td>{rotationPeriod}</td>
+                                    </tr>
+                                    <tr className="d-flex">
+                                        <th className="w-100">Diameter</th>
+                                        <td>{diameter}</td>
+                                    </tr>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </nav>
-    )
+            </nav>
+        )
+    }
 };
-
-export default RandomPlanet;
