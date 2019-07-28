11ffcfb238888b92db2039bd08fbf0f94565eae4
R-Mosolov <r.v.mosolov@ya.ru>
2019-07-28 20:03:02 +0300

    Lesson 57 is done

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -9,11 +9,7 @@ export default class RandomPlanet extends Component {
     swapiService = new SwapiService();
 
     state = {
-        id: null,
-        name: null,
-        population: null,
-        rotationPeriod: null,
-        diameter: null
+        planet: {}
     };
 
     constructor() {
@@ -21,22 +17,20 @@ export default class RandomPlanet extends Component {
         this.updatePlanet();
     }
 
+    onPlanetLoaded = (planet) => {
+        this.setState({
+            planet
+        })
+    };
+
     updatePlanet() {
-        const id = Math.floor(Math.random() * 15) + 2;
-        this.swapiService.getPlanet(id).then((planet) => {
-            this.setState({
-                id,
-                name: planet.name,
-                population: planet.population,
-                rotationPeriod: planet.rotation_period,
-                diameter: planet.diameter
-            });
-        });
+        const id = 12;
+        this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
     };
 
     render() {
 
-        const {id, name, population, rotationPeriod, diameter} = this.state;
+        const { planet: { id, name, population, rotationPeriod, diameter } } = this.state;
 
         return (
             <nav className="random-planet">
