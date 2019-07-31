73656701f416ee3a2f6937c502f67bf0fe06fec5
R-Mosolov <r.v.mosolov@ya.ru>
2019-07-31 22:45:11 +0300

    Lesson 69 is done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,11 +5,14 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
+import ErrorButton from '../error-button';
+import ErrorIndicator from "../error-indicator";
 
 export default class App extends Component {
 
     state = {
-        selectedPerson: 5
+        selectedPerson: 5,
+        hasError: false
     };
 
     onPersonSelected = (id) => {
@@ -18,12 +21,25 @@ export default class App extends Component {
         });
     };
 
+    componentDidCatch(error, errorInfo) {
+        console.log('componentDidCatch()');
+        this.setState({
+            hasError: true
+        });
+    }
+
     render() {
+
+        if (this.state.hasError) {
+            return <ErrorIndicator/>;
+        }
+
         return (
             <div className="container">
                 <Header/>
                 <h2 className="mt-3">Random Planet</h2>
                 <RandomPlanet/>
+                <ErrorButton/>
                 <h2 className="mt-3">Select a Person</h2>
                 <div className="d-lg-flex mb-3">
                     <ItemList onItemSelected={this.onPersonSelected}/>
