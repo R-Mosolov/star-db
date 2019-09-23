969b30130fa33c958a2093d135388df9f17b7746
R-Mosolov <r.v.mosolov@ya.ru>
2019-09-23 10:56:32 +0300

    Lesson 76 is done

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import ItemList from '../item-list/item-list';
-import PersonDetails from '../person-details/person-details';
+import ItemDetails from '../item-details/item-details';
 import SwapiService from '../../services/swapi-service';
 import Row from '../row';
 import ErrorBoundry from '../error-boundry';
@@ -13,7 +13,7 @@ export default class PeoplePage extends Component {
   swapiService = new SwapiService();
 
   state = {
-    selectedPerson: 3
+    selectedPerson: 11
   };
 
   onPersonSelected = (selectedPerson) => {
@@ -28,7 +28,7 @@ export default class PeoplePage extends Component {
         getData={this.swapiService.getAllPeople}>
 
         {(i) => (
-          `${i.name} (${i.gender}, ${i.birthYear})`
+          `${i.name} (${i.birthYear})`
         )}
 
       </ItemList>
@@ -36,7 +36,7 @@ export default class PeoplePage extends Component {
 
     const personDetails = (
       <ErrorBoundry>
-        <PersonDetails personId={this.state.selectedPerson} />
+        <ItemDetails itemId={this.state.selectedPerson} />
       </ErrorBoundry>
     );
 
